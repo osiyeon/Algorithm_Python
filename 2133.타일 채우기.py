822dc9b88f0b68d8ec8ee403f2335f3141c4fa03
osiyeon <osiyun0313@gmail.com>
2021-01-19 18:05:40 +0900

    2021.01.19

--- 2133.타일 채우기.py
+++ 2133.타일 채우기.py
@@ -1,19 +1,3 @@
-# N = int(input())
-
-# DP = [0]*(N+1)
-# for n in range(1, N+1):
-#     if n == 2:
-#         DP[n] = 3
-#     else:
-#         if n % 2 == 0:
-#             i = 2
-#             while i != n:
-#                 DP[n] += DP[i]*DP[n-i]
-#                 i += 2
-#             DP[n] += 2
-
-# print(DP)
-    
 N = int(input())
 
 DP = [0]*(N+1)
