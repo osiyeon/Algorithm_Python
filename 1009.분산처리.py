8a7fb154d5abb5e300e70c6cdbe158b60b82c4ba
osiyeon <osiyun0313@gmail.com>
2021-01-21 18:57:26 +0900

    2021.01.21

--- 1009.분산처리.py
+++ 1009.분산처리.py
@@ -1,14 +1,13 @@
-import math
-
 n = int(input())
 
 data = [list(map(int, input().split())) for _ in range(n)]
 
-data_count = []
 result = []
 for i in range(n):
-    data_count.append(data[i][0]** data[i][1])
-    result.append(data_count[i]%10)
+    if data[i][0] % 10 == 0:
+        result.append(10)
+    else:
+        result.append(pow(data[i][0],data[i][1], 10))
 
 for a in result:
     print(a)
